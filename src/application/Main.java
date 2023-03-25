adbbd1be3923d11d6826365e5b1997f1013815d5
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2023-03-25 18:02:31 -0300

    experience with comparator

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,23 @@
 package application;
 
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+    public static void main(String[] args) {
+        final List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        list.sort(Comparator.comparing(p -> p.getName().toUpperCase()));
+
+        for (Product product : list) {
+            System.out.println(product);
+        }
+    }
 }
